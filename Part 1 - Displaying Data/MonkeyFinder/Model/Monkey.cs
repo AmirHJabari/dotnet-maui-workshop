0579a741f1e1755b89fc16176552ab58e2156efc
Amir H. Jabari <amirhamzehjabari@gmail.com>
2022-08-27 00:47:10 +0430

    add monkey collection view

--- Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
@@ -2,4 +2,16 @@
 
 public class Monkey
 {
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Details { get; set; }
+    public string Image { get; set; }
+    public int Population { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
 }
