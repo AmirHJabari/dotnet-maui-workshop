a281e9908eedc7b694ee730af2fae72a869b5454
Amir H. Jabari <amirhamzehjabari@gmail.com>
2022-08-27 18:13:49 +0430

    better way of mvvm in maui

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,41 +1,13 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel : INotifyPropertyChanged
+public partial class BaseViewModel : ObservableObject
 {
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
     bool isBusy;
+
+    [ObservableProperty]
     string title;
 
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    public bool IsBusy 
-    {
-        get => isBusy;
-        set
-        {
-            if (isBusy.Equals(value)) return;
-
-            isBusy = value;
-            OnPropertyChanged();
-            OnPropertyChanged(nameof(IsNotBusy));
-        }
-    }
-
-    public string Title
-    {
-        get => title;
-        set
-        {
-            if (title.Equals(value)) return;
-
-            title = value;
-            OnPropertyChanged();
-        }
-    }
-
     public bool IsNotBusy => !IsBusy;
-
-    public void OnPropertyChanged([CallerMemberName] string name = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
 }
